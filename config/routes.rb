78117d0031882a2043057046dca29b1be6ccb735
Shashank Gupta <shashank_g@yahoo.in>
2014-08-20 16:43:23 +0530

    added associations2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :comments
 
   resources :posts do
-    resources :comments
+   resources :comments
   end
+   
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
