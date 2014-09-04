1251428044aaf253b7d45d3c4e41f723a42af722
Shashank Gupta <shashank_g@yahoo.in>
2014-09-04 13:54:54 +0530

    trying to add a new pic

--- config/application.rb
+++ config/application.rb
@@ -17,16 +17,11 @@ module Blog
 
 
   config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js|css.scss)\z/
+      if path =~ /\.(css|js|css.map|min.css|css.scss|js|min.js|js.coffee)\z/
         full_path = Rails.application.assets.resolve(path).to_path
         app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
           puts "including asset: " + full_path
           true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
       else
         false
       end
